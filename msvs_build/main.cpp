55951de0bfd7a1f7cdfa75fb960934a270951e93
Zakhar Stavitsky <zakhar.stavitsky@gmail.com>
2017-12-10 22:52:08 +0300

    Current status

--- msvs_build/main.cpp
+++ msvs_build/main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <cstdio>
 
 #include "Challenges/Hackerrank/CTCI.h"
 #include "Challenges/Hackerrank/Cpp.h"
@@ -15,13 +16,7 @@ using namespace hrnk::ctci;
 
 int main()
 {
-   common::timer t;
-   for(int i = 0; i < 1000000; i++)
-   {
-      anagrams::run(anagrams::solution_orig, false);
-   }
-   cout << t.get_duration_us() << endl;
-
+   printf("\");
    return 0;
 }
 
