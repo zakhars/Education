c731d3b150f506719ac9217d948f272d983f2070
Zakhar Stavitsky <zakhar.stavitsky@gmail.com>
2019-01-21 13:21:54 +0300

    Current progress

--- Challenges/Leetcode/LeetCodeAll.cpp
+++ Challenges/Leetcode/LeetCodeAll.cpp
@@ -2,39 +2,68 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <unordered_set>
 using namespace std;
 
 namespace leetcode
 {
    // Task: https://leetcode.com/problems/two-sum/
    // Time to first submit: 13
-   // Time to last submit: 13
-   // Number of submits: 1
-   // Errors: 2
-   // Debug: no
+   // Time to last submit: 120
+   // Number of submits: 10
+   // Errors: 5
+   // Debug: yes
    vector<int> twoSum(vector<int>& nums, int target) {
-      vector<int> indices;
+      unordered_set<int> hashed(begin(nums), end(nums));
       for (int i = 0; i < nums.size(); ++i)
       {
-         for (int j = i + 1; j < nums.size(); ++j)
+         auto second = target - nums[i];
+         if (hashed.count(second) != 0)
          {
-            if (nums[i] + nums[j] == target)
+            auto found_it = find(begin(nums) + i + 1, end(nums), second);
+            if (found_it != end(nums))
             {
-               indices.push_back(i);
-               indices.push_back(j);
-               return indices;
+               int j = static_cast<int>(distance(begin(nums), found_it));
+               return { i,j };
             }
          }
       }
-      return indices;
+      return {};
+   }
+
+   struct ListNode {
+      int val;
+      ListNode* next;
+      ListNode(int x) : val(x), next(nullptr) {}
+   };
+
+   // Task: https://leetcode.com/problems/add-two-numbers/
+   // Time to first submit: 0:00
+   // Time to last submit: 
+   // Number of submits: 
+   // Errors: 
+   // Debug: no
+   ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+   {
+      return {};
    }
 
 
 } // namespace leetcode
 
 
-int main_twoSum()
+int main()
 {
-
+   using namespace leetcode;
+   ListNode* l1 = new ListNode(2, new ListNode(4, (new ListNode(3)));
+   
+    
+   ListNode* l2;
+   auto sum = addTwoNumbers(l1, l2);
+   while (sum != nullptr)
+   {
+      cout << sum->val << ' ';
+      sum = sum->next;
+   }
    return 0;
 }
